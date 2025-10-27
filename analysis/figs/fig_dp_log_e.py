9895cff81a0b6c6153271935bf99171dece08f42
Todd Pataky <0todd0@gmail.com>
2025-10-27 19:58:07 +0900

    update figs 1-2

--- analysis/figs/fig_dp_log_e.py
+++ analysis/figs/fig_dp_log_e.py
@@ -31,44 +31,43 @@ colors  = plt.cm.gray( np.linspace(0,1,10)  )[:6]
 
 # one-sample case:
 ax     = axs[0]
-loc    = [(60,-0.2,0), (55,-1.1,-5), (50,-3.7,-30), (27,-4.1,-45), (15,-4.5,-61), (7.8,-3.9,-70)]
+loc    = [(25,-0.4,0), (25,-1.8,-5), (25,-5.3,-22), (25,-9.1,-40), (23,-14.5,-58), (14.5,-14.5,-67)]
 for pp,cc,ss,lc in zip(p1.T,colors,labels,loc): 
-    ax.plot( n1, np.log10(pp), color=cc, lw=2, zorder=1 )
+    ax.plot( n1, np.log(pp), color=cc, lw=2, zorder=1 )
     x,y,a = lc
     ax.text(x, y, ss, rotation=a, color=cc)
 vlines = [3, 4, 7, 13, 70]
 dx     = [-0.5, 0, 0, 0, 0]
-dy     = [0, 0, 0, 0, 1]
+dy     = [0, 0, 0, 2, 0]
 for vl,ddx,ddy in zip(vlines, dx, dy):
     ha = 'right' if vl==3 else 'left'
-    ax.plot([vl,vl], [-15,np.log10(0.05)], color='r', ls=':', zorder=0)
-    ax.text(vl+ddx, -4+ddy, fr'$n$ = {vl}', rotation=-90, ha=ha, va='top', color='r')
-ax.axhline( np.log10(0.05), color='r', ls='--', label=r'$\alpha$=0.05', zorder=0)
-ax.legend( bbox_to_anchor=(0.6,0.68), facecolor='w' )
+    ax.plot([vl,vl], [-15,np.log(0.05)], color='r', ls=':', zorder=0)
+    ax.text(vl+ddx, -11+ddy, fr'$n$ = {vl}', rotation=-90, ha=ha, va='top', color='r')
+ax.axhline( np.log(0.05), color='r', ls='--', label=r'$\alpha$=0.05', zorder=0)
+ax.legend( bbox_to_anchor=(0.5,0.78), facecolor='w' )
 
 
 
 # two-sample case:
 ax     = axs[1]
-loc    = [(60,-0.2,0), (65,-0.65,-3), (65,-1.7,-9), (60,-2.9,-19), (48,-4.6,-36), (22,-4.5,-55)]
+loc    = [(60,-0.4,0), (70,-1.5,-2), (70,-4.0,-7), (60,-6.7,-18), (50,-10.9,-25), (32,-14.1,-49)]
 for pp,cc,ss,lc in zip(p2.T,colors,labels,loc):
-    ax.plot( n2, np.log10(pp), color=cc, lw=2, zorder=1 )
+    ax.plot( n2, np.log(pp), color=cc, lw=2, zorder=1 )
     x,y,a = lc
     ax.text(x, y, ss, rotation=a, color=cc)
 vlines = [6, 10, 20, 46]
-dy     = [0, 0, 1.5, 1.5, 0]
-for ddy,vl in zip(dy,vlines):
-    ax.plot([vl,vl], [-15,np.log10(0.05)], color='r', ls=':', zorder=0)
-    ax.text(vl, -4+ddy, fr'$n$ = {vl}', rotation=-90, ha='left', va='top', color='r')
-ax.axhline( np.log10(0.05), color='r', ls='--', label=r'$\alpha$=0.05', zorder=0)
+for vl in vlines:
+    ax.plot([vl,vl], [-15,np.log(0.05)], color='r', ls=':', zorder=0)
+    ax.text(vl, -11, fr'$n$ = {vl}', rotation=-90, ha='left', va='top', color='r')
+ax.axhline( np.log(0.05), color='r', ls='--', label=r'$\alpha$=0.05', zorder=0)
 ax.set_yticklabels([])
 
 
 
 [ax.set_xlim( 0, 82 )  for ax in axs]
-[ax.set_ylim( -5, 0.5 )  for ax in axs]
+[ax.set_ylim( -15, 0.5 )  for ax in axs]
 [ax.set_xlabel(f'Sample size ($n$)', size=12)  for ax in axs]
-axs[0].set_ylabel(r'log$_{10}$ ( p-value )', size=12)
+axs[0].set_ylabel('log( p-value )', size=12)
 
 panel_labels = '(a) One-sample', '(b) Two-sample'
 [ax.text(0.02, 1.02, s, size=12, transform=ax.transAxes)  for ax,s in zip(axs,panel_labels)]
