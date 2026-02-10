4568be73c9b8a3534d27a8eefff208c5b918caab
0todd0@gmail.com <0todd0@gmail.com>
2026-02-10 22:28:53 +0900

    update new posthoc fig

--- analysis/figs_newapi/fig_oa_es_posthoc_new.py
+++ analysis/figs_newapi/fig_oa_es_posthoc_new.py
@@ -8,32 +8,9 @@ import esrot1d as e1d
 e1d.set_plot_style()
 
 
-# specify scenarios:
-# dc0 = e1d.stats.d_critical(20, dim=0, design='2sample')  # baseline scenario
 
-dc0 = e1d.stats.d_critical(20, dim=1, design='2sample', Q=101, fwhm=21.9)  # proposed baseline scenario
-dc1 = e1d.stats.d_critical(20, dim=1, design='1sample', Q=101, fwhm=21.9)
-
-dc2 = e1d.stats.d_critical(52, dim=1, design='2sample', Q=101, fwhm=21.9)
-dc3 = e1d.stats.d_critical(52, dim=1, design='1sample', Q=101, fwhm=21.9)
-
-dc4 = e1d.stats.d_critical(52, dim=1, design='2sample', Q=101, fwhm=73.3)
-dc5 = e1d.stats.d_critical(52, dim=1, design='1sample', Q=101, fwhm=73.3)
-
-
-# interpretations  = [['Very small',0.01], ['Small',0.2], ['Medium',0.5], ['Large',0.8], ['Very large',1.2], ['Huge',2.0]]
-# interpretations1 = [['Very small',0.13], ['Small',0.55], ['Medium',1.14], ['Large',1.75], ['Very large',2.59], ['Huge',4.13]]
-#
-# dc = e1d.stats.d_critical(20, dim=1, design='1sample', Q=101, fwhm=21.9)
-# # dc = e1d.stats.d_critical(20, dim=1, design='1sample', Q=101, fwhm=73.3)
-# dc = e1d.stats.d_critical(52, dim=1, design='2sample', Q=101, fwhm=73.3)
-
-# interpretations1 = list( zip( dc.keys(), dc.values() ) )
-
-
-
-def plot_interpretations(ax, dc, ylim=(-1,1)):
-    ymin,ymax = ylim
+def plot_interpretations(ax, dc, ymax=None):
+    ymax      = 1e6 if (ymax is None) else ymax
     # interps   = interpretations if type=='0d' else interpretations1
     interps   = dc.tolist()
     labels    = [i[0] for i in interps]
@@ -50,6 +27,7 @@ def plot_interpretations(ax, dc, ylim=(-1,1)):
         # if values[i] < -ymin:
         #     ax.axhline(-values[i], color=c, ls='-', zorder=0)
     ax.set_ylim( -0.3, ymax )
+
 
 
 
@@ -78,32 +56,51 @@ d      = e1d.stats.d_1sample(y1 - y2)
 
 
 
+# specify scenarios:
+# dc0 = e1d.stats.d_critical(20, dim=0, design='2sample')  # baseline scenario
+ns      = [20, 20,   52, 52,   52, 52]
+designs = ['2sample', '1sample'] * 3
+fwhms   = [21.9, 21.9,   21.9, 21.9,   73.3, 73.3]
 
 
-# PLOT:
+
+# plot:
 plt.close('all')
 fig,axs = plt.subplots(3, 2, figsize=(8,8), tight_layout=True)
 
-dcs = [dc0, dc1, dc2, dc3, dc4, dc5]
-for ax,dc in zip(axs.ravel(), dcs):
+
+for i,(ax,n,design,fwhm) in enumerate( zip(axs.ravel(), ns, designs, fwhms) ):
+    dc = e1d.stats.d_critical(n, dim=1, design=design, Q=101, fwhm=fwhm)  # proposed baseline scenario
     ax.plot( d, color='k' )
     ax.axhline(0, color='k', ls=':')
     
-    
-    
 
-    ylim = -0.3, 0.89
-    plot_interpretations(ax, dc, ylim=ylim)
+    
+    plot_interpretations(ax, dc, ymax=0.8)
     # ax.legend( fontsize=12, loc='upper left' )
     ax.set_xlim(0, 100)
-    # ax.set_xlabel('Time (%)', size=12)
-    # ax.set_ylabel("Effect size (Cohen's d-value)", size=12)
-    # # ax.grid(None)
-    # label = 'Post hoc functional rules of thumb based on THA dataset'
-    # ax.text(0.02, 0.95, label, transform=ax.transAxes, size=12)
+    ax.set_ylim(-0.3, 0.93)
+    
+    ax.text(0.02, 0.93, f'({chr(97+i)})  n={n}, {design}, fwhm={fwhm}%', transform=ax.transAxes)
 
 
-# # plt.savefig(  os.path.join(  os.path.dirname(__file__) , 'pdf', 'fig_oa_es_posthoc.pdf'  )  )
+
+
+axs[0,0].text(0.03, 0.78, 'PROPOSED BENCHMARK SCENARIO', transform=axs[0,0].transAxes, fontweight='bold') 
+axs[2,1].text(0.03, 0.83, 'ACTUAL SCENARIO', transform=axs[2,1].transAxes, fontweight='bold') 
+
+# [ax.text(0.1, 0.83, label, transform=ax.transAxes, fontweight='bold')  for ax,label in zip([axs[0,0], axs[2,1]], labels)]
+
+[ax.set_xlabel('Time (%)', size=12) for ax in axs[2]]
+[ax.set_ylabel('Effect size', size=12)  for ax in axs[:,0]]
+# [ax.set_ylabel(f'{s}\nEffect size', size=12)  for ax,s in zip(axs[:,0], ['n=20, fwhm=21.9%', 'n=52, fwhm=21.9%', 'n=52, fwhm=73.3%'])]
+[ax.set_xticklabels([])   for ax in axs[:2].ravel()]
+[ax.set_yticklabels([])   for ax in axs[:,1]]
+
+[ax.set_title(s)  for ax,s in zip(axs[0], ['2-sample', '1-sample'])]
+
+
+plt.savefig(  os.path.join(  os.path.dirname(__file__) , 'pdf', 'fig_oa_es_posthoc.pdf'  )  )
 plt.show()
 
 
