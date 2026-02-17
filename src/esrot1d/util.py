b942cc47d41a415c9a60ff117eb1b8c57f0749fb
0todd0@gmail.com <0todd0@gmail.com>
2026-02-17 12:07:08 +0900

    update examples nb

--- src/esrot1d/util.py
+++ src/esrot1d/util.py
@@ -28,20 +28,13 @@ def unique_sorted(x):
     return np.sort( np.unique(x) )
 
 
-def plot_critical_values(ax, type='0d', ylim=(-1,1)):
-    ymin,ymax = ylim
-    interps   = interpretations if type=='0d' else interpretations1
-    labels    = [i[0] for i in interps]
-    values    = [i[1] for i in interps]
-    values   += [100]
-    n         = len(values)
-    colors    = plt.cm.hot( np.linspace(0, 1, n+2) )[2:]
-    # oy        = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05]
-    for i,(s,c) in enumerate( zip(labels,colors) ):
-        if values[i] < ymax:
-            ax.axhline(values[i], color=c, ls='-', zorder=0)
+def plot_critical_values(ax, cv, ylim=(-1,1), colors=None):
+    if colors is None:
+        colors = plt.cm.jet( np.linspace(0, 1, len(cv)) )
+    ymin,ymax  = ylim
+    ax.axhline(0, color='0.7', ls=':', zorder=0)
+    for c,(key,value) in zip(colors, cv.items()):
+        if value < ymax:
+            ax.axhline(value, color=c, ls='-', zorder=0)
             bbox = dict(facecolor='w', edgecolor="0.5", pad=2, alpha=0.6)
-            ax.text( 80, values[i]+0.01, s, color=c, bbox=bbox, size=12 )
-        # if values[i] < -ymin:
-        #     ax.axhline(-values[i], color=c, ls='-', zorder=0)
-    ax.set_ylim( -0.3, ymax )
+            ax.text( 0.8, value+0.01, key, color=c, bbox=bbox, size=12 )
